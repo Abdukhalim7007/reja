3850a8646282e8dc56972412146af1d04d683e54
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-06-03 15:16:55 +0900

    feat: develop master router ejs page

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1 Kirish code lari
 app.use(express.static("public"));
@@ -13,20 +22,29 @@ app.use(express.urlencoded({extended: true}));
 //2:Sessions code
 
 // 3 views code  backend ni ichida frontend ni yasaymiz
-app.set("veiws", "views");
+app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
-app.get("/hello", function (req, res) {
-    res.send(`<h1>HELLO WORLD</h1>`);
-});
-app.get("/gift", function (req, res) {
-    res.send(`<h1>Siz sovgalar bolimidasiz</h1>`);
-});
+// app.get("/hello", function (req, res) {
+//     res.send(`<h1>HELLO WORLD</h1>`);
+// });
+// app.get("/gift", function (req, res) {
+//     res.send(`<h1>Siz sovgalar bolimidasiz</h1>`);
+// });
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success" });
+    //TODO: code with db here
+    res.json({status: "success"});     
 });
+
+app.get("/master", (req, res) => {
+   res.render("master", {user: user});
+});
+
+// app.post("/create-item", (req, res) => {
+//     console.log(req.body);
+//     res.json({test: "success" });
+// });
 app.get("/", function (req, res) {
     res.render("harid");
 
