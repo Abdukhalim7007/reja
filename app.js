4adf54bde052d44303cdfe089999abee46394456
Abdukhalim7007 <abduhalim.tech@gmail.com>
2025-06-09 18:54:49 +0900

    fix: refactoring

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) =>{
+    if(err){
+        console.log("ERROR:", err );
+    }else{
+        user = JSON.parse(data)
+    }
+});
+
+
+// MONGOBD cennect
+//1: Kirish codesss
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//2 Session code
+//3 View code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4 Routing code
+app.post("/create-item", (req,res) =>{
+    console.log(req);
+    res.json({test: "success"});
+});
+
+app.get('/master', (req, res) => {
+    res.render("master", { user: user })
+});
+
+app.get("/", function(req, res){
+    res.render("reja");
+
+});
+
+ 
+
+ module.exports = app;
--- server.js
+++ server.js
@@ -1,48 +1,33 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+ 
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) =>{
-    if(err){
-        console.log("ERROR:", err );
-    }else{
-        user = JSON.parse(data)
+
+let db;
+  const connectionString = "mongodb+srv://Yohan:pRbGxWjIuY3xa9NH@cluster0.dogmyac.mongodb.net/Reja?retryWrites=true&w=majority";
+
+
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}, (err, client) => {
+    if(err) console.log("ERROR on connection Mongodb");
+    else{
+        console.log("Mongodb connection succeed");
+        console.log(client);
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+        console.log(`The server is successfully running on port:, ${PORT}, http://localhost:${PORT}`
+         );
+        });
     }
-});
+ }
+);
 
-//1: Kirish codesss
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-//2 Session code
-//3 View code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4 Routing code
-app.post("/create-item", (req,res) =>{
-    console.log(req);
-    res.json({test: "success"});
-});
-
-app.get('/master', (req, res) => {
-    res.render("master", { user: user })
-});
-
-app.get("/", function(req, res){
-    res.render("harid");
-
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`The server is successfully running on port:, ${PORT} `);
-});
+ 
 
  
